6c85d6aa4f0a93c1b223282292ebfd916944200c
mohammwed munifi <hamadaaaaaqqqqq@gmail.com>
2022-12-30 17:49:58 +0200

    add

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Homecontroller;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,5 +28,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[Homecontroller::class,'index'])->name('Home');
 
 Route::get('/pages/{name}',[Homecontroller::class,'show'])->name('pages');
+
+Route::view('/dashboard','layouts.dashborad');
 
 
